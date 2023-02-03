8ed7f65838e3e8b2061b7312f28987bac91d38c2
timohuennebeck <hello@timohuennebeck.com>
2023-02-03 16:49:11 +0100

    change data

--- seeds_data/dictionaries.js
+++ seeds_data/dictionaries.js
@@ -1,47 +1,56 @@
 module.exports = [
     {
         id: 1,
-        name: "Beginner 101",
+        name: "Fondamentaux",
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ratione minima cum accusantium autem? Qui libero exercitationem fugiat dolorem, rerum accusamus ut officiis praesentium obcaecati id cum incidunt dicta vero!",
         rating: 3,
         image_url: "https://i.imgur.com/Y4hgndp.png",
-        language: "Spanish",
+        language: "French",
     },
     {
         id: 2,
-        name: "Intermediate 101",
+        name: "Intermédiaire 101",
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ratione minima cum accusantium autem? Qui libero exercitationem fugiat dolorem, rerum accusamus ut officiis praesentium obcaecati id cum incidunt dicta vero!",
         rating: 5,
         image_url: "https://i.imgur.com/a3xaq9j.png",
-        language: "Spanish",
+        language: "French",
     },
     {
         id: 3,
-        name: "Advanced 101",
+        name: "Fundamentos",
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ratione minima cum accusantium autem? Qui libero exercitationem fugiat dolorem, rerum accusamus ut officiis praesentium obcaecati id cum incidunt dicta vero!",
         rating: 4,
         image_url: "https://i.imgur.com/zukvUzT.png",
-        language: "German",
+        language: "Spanish",
     },
     {
         id: 4,
-        name: "Subjunctive Triggers",
+        name: "Intermedio 101",
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ratione minima cum accusantium autem? Qui libero exercitationem fugiat dolorem, rerum accusamus ut officiis praesentium obcaecati id cum incidunt dicta vero!",
         rating: 5,
         image_url: "https://i.imgur.com/xxtk9kN.png",
-        language: "French",
+        language: "Spanish",
     },
     {
         id: 5,
-        name: "Introducing Yourself",
+        name: "Grundlagen",
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ratione minima cum accusantium autem? Qui libero exercitationem fugiat dolorem, rerum accusamus ut officiis praesentium obcaecati id cum incidunt dicta vero!",
         rating: 4,
         image_url: "https://i.imgur.com/43FwnrE.png",
-        language: "French",
+        language: "German",
+    },
+    {
+        id: 6,
+        name: "Fortgeschrittene 101",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ratione minima cum accusantium autem? Qui libero exercitationem fugiat dolorem, rerum accusamus ut officiis praesentium obcaecati id cum incidunt dicta vero!",
+        rating: 4,
+        image_url: "https://i.imgur.com/43FwnrE.png",
+        language: "German",
     },
 ];
