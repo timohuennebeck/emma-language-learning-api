7735b71c5660684f9f35a17032efa02b23b66e35
timohuennebeck <hello@timohuennebeck.com>
2023-02-08 16:42:43 +0100

    change language moel

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post("/openai", async (req, res) => {
 
     try {
         const response = await openai.createCompletion({
-            model: "text-davinci-003",
+            model: "text-davinci-002",
             prompt: message,
             temperature: 0.95,
             max_tokens: 256,
